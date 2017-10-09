8d5b908cb6f2ac162b40e64e9f23f2860bc02a03
r21nomi <ryotakosu@gmail.com>
2017-10-09 17:52:20 +0900

    Add log for RDS info

--- datastore/db.go
+++ datastore/db.go
@@ -4,6 +4,7 @@ import(
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	"os"
+	"log"
 )
 
 var Db *gorm.DB
@@ -13,6 +14,10 @@ func init() {
 	var RDS_USERNAME = os.Getenv("RDS_USERNAME")
 	var RDS_DB_NAME = os.Getenv("RDS_DB_NAME")
 	var RDS_PASSWORD = os.Getenv("RDS_PASSWORD")
+
+	log.Printf("RDS_USERNAME: %s\n\n", RDS_USERNAME)
+	log.Printf("RDS_DB_NAME: %s\n\n", RDS_DB_NAME)
+	log.Printf("RDS_PASSWORD: %s\n\n", RDS_PASSWORD)
 
 	var err error
 	Db, err = gorm.Open(DBMS, "user=" + RDS_USERNAME + " dbname=" + RDS_DB_NAME + " password=" + RDS_PASSWORD + " sslmode=disable")
