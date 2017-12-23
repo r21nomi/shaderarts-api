f79b7154c04232619c9e3ff83456d3187582b84d
r21nomi <ryotakosu@gmail.com>
2017-12-24 01:17:59 +0900

    Add dependencies

---
+++ vendor/firebase.google.com/go/firebase.go
@@ -0,0 +1,135 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package firebase is the entry point to the Firebase Admin SDK. It provides functionality for initializing App
+// instances, which serve as the central entities that provide access to various other Firebase services exposed
+// from the SDK.
+package firebase
+
+import (
+	"errors"
+
+	"cloud.google.com/go/firestore"
+
+	"firebase.google.com/go/auth"
+	"firebase.google.com/go/iid"
+	"firebase.google.com/go/internal"
+	"firebase.google.com/go/storage"
+
+	"os"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/transport"
+)
+
+var firebaseScopes = []string{
+	"https://www.googleapis.com/auth/cloud-platform",
+	"https://www.googleapis.com/auth/datastore",
+	"https://www.googleapis.com/auth/devstorage.full_control",
+	"https://www.googleapis.com/auth/firebase",
+	"https://www.googleapis.com/auth/identitytoolkit",
+	"https://www.googleapis.com/auth/userinfo.email",
+}
+
+// Version of the Firebase Go Admin SDK.
+const Version = "2.3.0"
+
+// An App holds configuration and state common to all Firebase services that are exposed from the SDK.
+type App struct {
+	creds         *google.DefaultCredentials
+	projectID     string
+	storageBucket string
+	opts          []option.ClientOption
+}
+
+// Config represents the configuration used to initialize an App.
+type Config struct {
+	ProjectID     string
+	StorageBucket string
+}
+
+// Auth returns an instance of auth.Client.
+func (a *App) Auth(ctx context.Context) (*auth.Client, error) {
+	conf := &internal.AuthConfig{
+		Creds:     a.creds,
+		ProjectID: a.projectID,
+		Opts:      a.opts,
+		Version:   Version,
+	}
+	return auth.NewClient(ctx, conf)
+}
+
+// Storage returns a new instance of storage.Client.
+func (a *App) Storage(ctx context.Context) (*storage.Client, error) {
+	conf := &internal.StorageConfig{
+		Opts:   a.opts,
+		Bucket: a.storageBucket,
+	}
+	return storage.NewClient(ctx, conf)
+}
+
+// Firestore returns a new firestore.Client instance from the https://godoc.org/cloud.google.com/go/firestore
+// package.
+func (a *App) Firestore(ctx context.Context) (*firestore.Client, error) {
+	if a.projectID == "" {
+		return nil, errors.New("project id is required to access Firestore")
+	}
+	return firestore.NewClient(ctx, a.projectID, a.opts...)
+}
+
+// InstanceID returns an instance of iid.Client.
+func (a *App) InstanceID(ctx context.Context) (*iid.Client, error) {
+	conf := &internal.InstanceIDConfig{
+		ProjectID: a.projectID,
+		Opts:      a.opts,
+	}
+	return iid.NewClient(ctx, conf)
+}
+
+// NewApp creates a new App from the provided config and client options.
+//
+// If the client options contain a valid credential (a service account file, a refresh token file or an
+// oauth2.TokenSource) the App will be authenticated using that credential. Otherwise, NewApp attempts to
+// authenticate the App with Google application default credentials.
+func NewApp(ctx context.Context, config *Config, opts ...option.ClientOption) (*App, error) {
+	o := []option.ClientOption{option.WithScopes(firebaseScopes...)}
+	o = append(o, opts...)
+
+	creds, err := transport.Creds(ctx, o...)
+	if err != nil {
+		return nil, err
+	}
+
+	if config == nil {
+		config = &Config{}
+	}
+
+	var pid string
+	if config.ProjectID != "" {
+		pid = config.ProjectID
+	} else if creds.ProjectID != "" {
+		pid = creds.ProjectID
+	} else {
+		pid = os.Getenv("GCLOUD_PROJECT")
+	}
+
+	return &App{
+		creds:         creds,
+		projectID:     pid,
+		storageBucket: config.StorageBucket,
+		opts:          o,
+	}, nil
+}
---
+++ vendor/firebase.google.com/go/integration/internal/internal.go
@@ -0,0 +1,81 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package internal contains utilities for running integration tests.
+package internal
+
+import (
+	"encoding/json"
+	"go/build"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+const certPath = "integration_cert.json"
+const apiKeyPath = "integration_apikey.txt"
+
+// Resource returns the absolute path to the specified test resource file.
+func Resource(name string) string {
+	p := []string{build.Default.GOPATH, "src", "firebase.google.com", "go", "testdata", name}
+	return filepath.Join(p...)
+}
+
+// NewTestApp creates a new App instance for integration tests.
+//
+// NewTestApp looks for a service account JSON file named integration_cert.json
+// in the testdata directory. This file is used to initialize the newly created
+// App instance.
+func NewTestApp(ctx context.Context) (*firebase.App, error) {
+	pid, err := ProjectID()
+	if err != nil {
+		return nil, err
+	}
+	config := &firebase.Config{
+		StorageBucket: pid + ".appspot.com",
+	}
+	return firebase.NewApp(ctx, config, option.WithCredentialsFile(Resource(certPath)))
+}
+
+// APIKey fetches a Firebase API key for integration tests.
+//
+// APIKey reads the API key string from a file named integration_apikey.txt
+// in the testdata directory.
+func APIKey() (string, error) {
+	b, err := ioutil.ReadFile(Resource(apiKeyPath))
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(b)), nil
+}
+
+// ProjectID fetches a Google Cloud project ID for integration tests.
+func ProjectID() (string, error) {
+	b, err := ioutil.ReadFile(Resource(certPath))
+	if err != nil {
+		return "", err
+	}
+	var serviceAccount struct {
+		ProjectID string `json:"project_id"`
+	}
+	if err := json.Unmarshal(b, &serviceAccount); err != nil {
+		return "", err
+	}
+	return serviceAccount.ProjectID, nil
+}
